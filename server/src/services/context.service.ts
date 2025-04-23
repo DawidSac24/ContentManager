a8a583406fc7a204fda28104fff927f9dcdca728
DawidSac24 <dawidsac24@gmail.com>
2025-04-23 20:15:21 +0200

    adding the contexts getByid, put, and delete routes

--- server/src/services/context.service.ts
+++ server/src/services/context.service.ts
@@ -29,6 +29,29 @@ export class ContextsService {
     return results;
   }
 
+  public static getById(id: number): Context | null {
+    const req = `SELECT * FROM contexts WHERE id = ?`;
+    let context;
+
+    try {
+      context = DB.prepare(req).get(id);
+    } catch (error) {
+      LoggerService.error(error);
+      throw new Error("Internal Error");
+    }
+
+    if (!context) {
+      return null;
+    }
+
+    return {
+      id: context.id,
+      name: context.name,
+      icon_id: context.icon_id,
+      deleted: context.deleted,
+    };
+  }
+
   public static insert(newContext: NewContext): Context | null {
     const req = `INSERT INTO contexts (name, icon_id, deleted) 
            VALUES (?, ?, false)`;
@@ -50,5 +73,46 @@ export class ContextsService {
       icon_id: newContext.icon_id ?? 1,
       deleted: false,
     };
+  }
+
+  public static update(context: Context): Context | null {
+    const req = `UPDATE contexts SET name = ?, icon_id = ?, deleted = ? WHERE id = ?`;
+    let info;
+
+    try {
+      info = DB.prepare(req).run(
+        context.name,
+        context.icon_id ?? 1, // default to 1 if not provided
+        context.deleted ? 1 : 0,
+        context.id
+      );
+    } catch (error) {
+      LoggerService.error(error);
+      throw new Error("Internal Error");
+    }
+
+    if (info.changes === 0) {
+      return null;
+    }
+
+    return {
+      id: context.id,
+      name: context.name,
+      icon_id: context.icon_id ?? 1,
+      deleted: context.deleted,
+    };
+  }
+
+  public static delete(id: number): boolean {
+    const req = `DELETE FROM contexts WHERE id = ?`;
+    let info;
+
+    try {
+      info = DB.prepare(req).run(id);
+    } catch (error) {
+      LoggerService.error(error);
+      throw new Error("Internal Error");
+    }
+    return true;
   }
 }
