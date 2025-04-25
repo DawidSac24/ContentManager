edb5272daef9122fcb0d6b175dfae8e233a36f9d
DawidSac24 <dawidsac24@gmail.com>
2025-04-25 18:54:11 +0200

    Changing all the project to adapt it for a chrome extension - V0.2

--- server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const PORT = process.env.PORT || 5000;
-
-// starts the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
--- server/src/services/db.service.ts
+++
@@ -1,8 +0,0 @@
-// Il ne faut PAS MODIFIER le chemin du fichier de base de données.
-// Si le fichier n'est pas trouvé au lancement du programme,
-// assurez vous d'avoir correctement exécuté le script d'installation du projet,
-const DB = require("better-sqlite3")(
-  "C:/dev/projects/ContextManager/server/db/project.db"
-);
-
-export { DB };
